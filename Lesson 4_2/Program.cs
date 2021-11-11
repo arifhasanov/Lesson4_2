c1228a54d88c593ebb53c2b1a3c72e1af871920f
Arif Hasanov <arifhasanov@yahoo.com>
2021-11-11 10:58:36 +0100

    Done

---
+++ Lesson 4_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lesson_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter some numbers separated with spaces ' ' and then press ENTER");
+            string s = Console.ReadLine();
+
+            string[] subs = s.Split(' ');
+            int total = 0;
+
+            foreach (var sub in subs)
+            {
+                int number = 0;
+                if(int.TryParse(sub, out number))
+                {
+                    total += number;
+                }
+            }
+
+            Console.WriteLine($"Total of extracted numbers is: {total}");
+        }
+    }
+}
